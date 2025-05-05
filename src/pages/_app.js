26e4dcf881f46a180698f0cff52c1005f0b030a8
Emirhan Tandoğan <emirhantandogann@gmail.com>
2025-05-05 13:51:14 +0300

    navbar menu bug in small screen fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import "@/styles/globals.css";
 import { useEffect, useState } from 'react';
 import '../styles/globals.css';
@@ -7,5 +6,9 @@ import { fetchCart } from '../lib/api';
 
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap.bundle.min.js')
+  }, [])
+
   return <Component {...pageProps} />;
 }
